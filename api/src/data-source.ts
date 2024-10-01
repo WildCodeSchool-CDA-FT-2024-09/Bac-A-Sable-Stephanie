92f65164b7d047249a65b2ccc214ffae4f333de2
brewost <156411525+brewost@users.noreply.github.com>
2024-10-01 12:39:34 +0200

    add in entities to data-source.ts

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource } from "typeorm";
 import Repo from "./repos/repo.entity";
+import Status from "./status/status.entity";
+import Lang from "./langs/lang.entity";
 
 const AppDataSource = new DataSource({
   type: "sqlite",
   database: "database.sqlite",
   synchronize: true,
   logging: true,
-  entities: [Repo],
+  entities: [Repo, Status, Lang],
   migrations: [],
 });
 
